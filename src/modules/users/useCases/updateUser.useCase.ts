54be69660acd381583bfef8a8f317f602d2a5fd6
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-09-01 22:03:58 -0300

    feat: update execute

--- src/modules/users/useCases/updateUser.useCase.ts
+++ src/modules/users/useCases/updateUser.useCase.ts
@@ -1,11 +1,18 @@
 import { inject, injectable } from 'tsyringe';
+import { AppError } from '../../../shared/errors/AppError';
 import IUsersService from '../services/IUsersService';
 
 @injectable()
 export class UpdateUserUseCase {
   constructor(@inject('UsersService') private usersService: IUsersService) {}
 
-  async execute(userId: string) {
-    return this.usersService.updateUser(userId);
+  async execute(userId: string, name: string, email: string, password: string) {
+    const userToUpdate = await this.usersService.findById(userId);
+
+    if (userToUpdate) {
+      throw new AppError('User not found!', 404);
+    }
+
+    return this.usersService.updateUser(userId, name, email, password);
   }
 }
