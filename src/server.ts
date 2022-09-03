c01bf3aa5e6f38858febf8b273b84f8e8699270a
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-09-03 14:36:39 -0300

    feat: add app connection error treatment

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,29 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import { AppDataSource } from './data-source';
 import { router } from './routes';
+import logging from './shared/config/logging';
+import { AppError } from './shared/errors/AppError';
 
 AppDataSource.initialize().then(() => {
   const app = express();
   app.use(express.json());
   app.use(router);
-  app.use(
-    (err: Error, request: Request, response: Response, next: NextFunction) => {
-      // if (err instanceof AppError) {
-      //   return response.status(err.statusCode).json({
-      //     message: err.message,
-      //   });
-      // }
-
-      return response.status(500).json({
-        status: 'error',
-        message: `Internal server error - ${err.message}`,
+  app.use((err: Error, request: Request, response: Response) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
       });
     }
-  );
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  });
 
   const port = process.env.PORT;
 
   return app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    logging.info(`Server is running on port ${port}`);
   });
 });
