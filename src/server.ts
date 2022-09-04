c10097886adf275ad243a7d797e80961270c8374
Guilherme Albino de Andrade <guilhermealbino33@gmail.com>
2022-09-04 16:37:02 -0300

    fix: error message

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { AppDataSource } from './data-source';
 import { router } from './routes';
 import logging from './shared/config/logging';
@@ -8,18 +8,21 @@ AppDataSource.initialize().then(() => {
   const app = express();
   app.use(express.json());
   app.use(router);
-  app.use((err: Error, request: Request, response: Response) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
+  app.use(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+      if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+          message: err.message,
+        });
+      }
+
+      return response.status(500).json({
+        status: 'error',
+        message: `Internal server error - ${err.message}`,
       });
     }
-
-    return response.status(500).json({
-      status: 'error',
-      message: `Internal server error - ${err.message}`,
-    });
-  });
+  );
 
   const port = process.env.PORT;
 
